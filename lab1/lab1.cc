1f533c51a8dd99380760c7fadb9082075bcd9605
dasbd72 <twbd723@gmail.com>
2024-09-13 21:38:15 +0800

    refactor: turn lab1 into solver class

--- lab1/lab1.cc
+++ lab1/lab1.cc
@@ -63,15 +63,34 @@
 typedef unsigned long long ull;
 typedef long double ld;
 
-const ull half_max_ull = std::numeric_limits<ull>::max() / 2;
-int rank = 0, size = 1;
+class Solver {
+   public:
+    Solver() {}
+    int solve(int argc, char** argv);
 
-inline void partial_pixels(ull r, ull k, ull start, ull end, ull& pixels);
-inline void finalize_pixels(ull k, ull square, ull& pixels);
-void exec_seq(ull r, ull k);
-void exec_mpi(ull r, ull k);
+   private:
+    const ull half_max_ull = std::numeric_limits<ull>::max() / 2;
+
+    int rank = 0;
+    int size = 1;
+    ull r;
+    ull k;
+    ull sq_r;
+    ull hf_x;
+    ull square;
+
+    inline void exec_seq();
+    inline void exec_mpi();
+    inline void partial_pixels(ull start, ull end, ull& pixels);
+    inline void finalize_pixels(ull& pixels);
+};
 
 int main(int argc, char** argv) {
+    Solver solver;
+    return solver.solve(argc, argv);
+}
+
+int Solver::solve(int argc, char** argv) {
     std::ios::sync_with_stdio(0);
     std::cin.tie(0);
     std::cout.tie(0);
@@ -79,22 +98,78 @@ int main(int argc, char** argv) {
         std::cerr << "must provide exactly 2 arguments!\n";
         return 1;
     }
-    ull r = atoll(argv[1]);
-    ull k = atoll(argv[2]);
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    r = atoll(argv[1]);
+    k = atoll(argv[2]);
+    // Optimization: Compute first and reuse the square of radius
+    sq_r = r * r;
+    hf_x = ceil(sqrtl(sq_r / 2));    // End of all tasks
+    square = k - (hf_x * hf_x) % k;  // Value to be deducted after multipling pixels by 2
+
     if (size == 1) {
         if (rank != 0)
             return 0;
-        exec_seq(r, k);
+        exec_seq();
     } else {
-        exec_mpi(r, k);
+        exec_mpi();
     }
     MPI_Finalize();
+    return 0;
 }
 
-inline void partial_pixels(ull r, ull k, ull start, ull end, ull& pixels) {
+inline void Solver::exec_seq() {
+    TIMING_START(seq_all);
+    ull pixels = 0;
+    partial_pixels(0, hf_x, pixels);
+    finalize_pixels(pixels);
+    std::cout << pixels << "\n";
+    TIMING_END(seq_all);
+}
+
+inline void Solver::exec_mpi() {
+    TIMING_START(mpi_all);
+    // Optimization: Different task size for each process
+    const double factor = 0.08;
+    const double sum_weights = size * 1.0 + factor * (size - 1) * size / 2;  // Sum of weights
+    ull pivots[size + 1] = {};
+    pivots[0] = 0;
+    for (int i = 1; i < size; i++)
+        pivots[i] = pivots[i - 1] + ceil((1.0 + (size - i) * factor) / sum_weights * hf_x);
+    pivots[size] = hf_x;
+    const ull local_start = pivots[rank];
+    const ull local_end = pivots[rank + 1];
+    ull pixels = 0;
+    ull* remote_pixels = nullptr;
+    ull local_pixels = 0;
+    TIMING_START(mpi_loop);
+    partial_pixels(local_start, local_end, local_pixels);
+    TIMING_END(mpi_loop);
+    if (rank == 0)
+        remote_pixels = (ull*)malloc(sizeof(ull) * size);  // Allocate buffer
+    TIMING_START(mpi_gather);
+    MPI_Gather(&local_pixels, 1, MPI_UNSIGNED_LONG_LONG, remote_pixels, 1, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        TIMING_END(mpi_gather);
+    }
+    if (rank == 0) {
+        for (int i = 0; i < size; i++) {
+            if (pixels > k)
+                pixels %= k;
+            pixels += remote_pixels[i];
+        }
+        finalize_pixels(pixels);
+        std::cout << pixels << "\n";
+    }
+    if (rank == 0) {
+        TIMING_END(mpi_all);
+    }
+}
+
+inline void Solver::partial_pixels(ull start, ull end, ull& pixels) {
     const ull sq_r = r * r;
     ull pxls = 0;
     ull x = start;
@@ -136,7 +211,7 @@ inline void partial_pixels(ull r, ull k, ull start, ull end, ull& pixels) {
     pixels = pxls;
 }
 
-inline void finalize_pixels(ull k, ull square, ull& pixels) {
+inline void Solver::finalize_pixels(ull& pixels) {
     if (pixels > half_max_ull)
         pixels %= k;
     pixels *= 2;
@@ -146,59 +221,4 @@ inline void finalize_pixels(ull k, ull square, ull& pixels) {
     if (pixels > half_max_ull)
         pixels %= k;
     pixels = (4 * pixels) % k;
-}
-
-void exec_seq(ull r, ull k) {
-    TIMING_START(seq_all);
-    const ull sq_r = r * r;
-    const ull hf_x = ceil(sqrtl(sq_r / 2));
-    const ull square = k - (hf_x * hf_x) % k;
-    ull pixels = 0;
-    partial_pixels(r, k, 0, hf_x, pixels);
-    finalize_pixels(k, square, pixels);
-    std::cout << pixels << "\n";
-    TIMING_END(seq_all);
-}
-
-void exec_mpi(ull r, ull k) {
-    TIMING_START(mpi_all);
-    // Optimization: Compute first and reuse the square of radius
-    const ull sq_r = r * r;
-    const ull hf_x = ceil(sqrtl(sq_r / 2));    // End of all tasks
-    const ull square = k - (hf_x * hf_x) % k;  // Value to be deducted after multipling pixels by 2
-    // Optimization: Different task size for each process
-    const double factor = 0.08;
-    const double sum_weights = size * 1.0 + factor * (size - 1) * size / 2;  // Sum of weights
-    ull pivots[size + 1] = {};
-    pivots[0] = 0;
-    for (int i = 1; i < size; i++)
-        pivots[i] = pivots[i - 1] + ceil((1.0 + (size - i) * factor) / sum_weights * hf_x);
-    pivots[size] = hf_x;
-    const ull local_start = pivots[rank];
-    const ull local_end = pivots[rank + 1];
-    ull pixels = 0;
-    ull* remote_pixels = nullptr;
-    ull local_pixels = 0;
-    TIMING_START(mpi_loop);
-    partial_pixels(r, k, local_start, local_end, local_pixels);
-    TIMING_END(mpi_loop);
-    if (rank == 0)
-        remote_pixels = (ull*)malloc(sizeof(ull) * size);  // Allocate buffer
-    TIMING_START(mpi_gather);
-    MPI_Gather(&local_pixels, 1, MPI_UNSIGNED_LONG_LONG, remote_pixels, 1, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
-    if (rank == 0) {
-        TIMING_END(mpi_gather);
-    }
-    if (rank == 0) {
-        for (int i = 0; i < size; i++) {
-            if (pixels > k)
-                pixels %= k;
-            pixels += remote_pixels[i];
-        }
-        finalize_pixels(k, square, pixels);
-        std::cout << pixels << "\n";
-    }
-    if (rank == 0) {
-        TIMING_END(mpi_all);
-    }
 }
