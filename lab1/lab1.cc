5d7761c57bab9462e6965a6c18d7dfcee354ef33
dasbd72 <twbd723@gmail.com>
2024-09-14 13:15:22 +0800

    feat: added purbula method to tune pivot

--- lab1/lab1.cc
+++ lab1/lab1.cc
@@ -144,13 +144,51 @@ inline void Solver::param_init() {
 
     // Optimization: Different task size for each process
     if (size > 1) {
+        // The task separating problem
+        // We have p,r > 0, r >= p
+        // We want to separate p to r into x_0, x_1, x_{k+1} pivots
+        // y_i = ceil(sqrtl(r^2-x_i^2)), j belongs to [0, k+1]
+        // Where x_0 = p and x_{k+1} = r
+        // Let A_j = (x_{j+1} - x_j) * (y_j - y_{j+1}), j belongs to [0, k]
+        // Give the best way to let A_n ~= A_m for all n, m belongs to [0, k]
+
         pivots = new ull[sizeof(ull) * (size + 1)];
-        const double factor = 0.62;
-        const double sum_weights = size * 1.0 + factor * (size - 1) * size / 2;  // Sum of weights
-        pivots[0] = hf_x;
-        for (int i = 1; i < size; i++)
-            pivots[i] = pivots[i - 1] + ceil((1.0 + (size - i) * factor) / sum_weights * (r - hf_x));
-        pivots[size] = r;
+        const int method = 1;
+
+        if (method == 0) {
+            // Arithmetic sequence method
+            const double factor = 0.62;
+            const double sum_weights = size * 1.0 + factor * (size - 1) * size / 2;  // Sum of weights
+            pivots[0] = hf_x;
+            for (int i = 1; i < size; i++) {
+                const double weight = 1.0 + (size - i) * factor;
+                pivots[i] = pivots[i - 1] + ceil(weight / sum_weights * (r - hf_x));
+            }
+            pivots[size] = r;
+        } else {
+            // The purbula method
+            const double a = -0.065;
+            const double m = (double)size / 7;
+            double w[size];
+            double min_w = std::numeric_limits<double>::max();
+            double sum_w = 0.0;
+            for (int i = 0; i < size; i++) {
+                w[i] = a * (i - m) * (i - m);
+                min_w = std::min(min_w, w[i]);
+            }
+            for (int i = 0; i < size; i++) {
+                w[i] = w[i] - min_w + 1;
+                sum_w += w[i];
+                if (rank == 0) {
+                    DEBUG_MSG(i << " " << w[i]);
+                }
+            }
+            pivots[0] = hf_x;
+            for (int i = 1; i < size; i++) {
+                pivots[i] = pivots[i - 1] + ceil(w[i - 1] / sum_w * (r - hf_x));
+            }
+            pivots[size] = r;
+        }
     }
 }
 
