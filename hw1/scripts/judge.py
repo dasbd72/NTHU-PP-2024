8eb8098fa7fd74fcf81a26132b9ed6fc526edb55
dasbd72 <twbd723@gmail.com>
2024-09-17 10:32:28 +0800

    feat: use temporary judge directory

--- hw1/scripts/judge.py
+++ hw1/scripts/judge.py
@@ -1,5 +1,6 @@
 import os
 import signal
+import shutil
 import time
 
 from .build import build
@@ -8,6 +9,18 @@ from .build import build
 def signal_int(signum, frame):
     print("Exiting...")
     exit(0)
+
+
+def copy_move():
+    # Copy files to a temporary directory and change directory
+    judge_dir = os.path.expanduser("~/.tmp.judge")
+    shutil.rmtree(judge_dir, ignore_errors=True)
+    os.mkdir(judge_dir)
+    shutil.copy("Makefile", judge_dir)
+    shutil.copy("modules.list", judge_dir)
+    shutil.copy("hw1.cc", judge_dir)
+    os.chdir(judge_dir)
+    print(f"Changed directory to {judge_dir}")
 
 
 def judge_loop():
@@ -23,4 +36,5 @@ def judge_loop():
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_int)
+    copy_move()
     judge_loop()
