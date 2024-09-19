be0e82ae1d36cab287c9afae641c2fc5b3914769
dasbd72 <twbd723@gmail.com>
2024-09-19 22:18:20 +0800

    feat: added LSAN_OPTIONS export for sanitizing

--- hw1-mpi-odd-even-sort/env/env.template.sh
+++ hw1-mpi-odd-even-sort/env/env.template.sh
@@ -2,3 +2,9 @@
 export TIMING=0
 export DEBUG=0
 export SANITIZE=0
+
+if [ $SANITIZE -eq 1 ]; then
+    export LSAN_OPTIONS=verbosity=1:log_threads=1
+elif [ $LSAN_OPTIONS ]; then
+    unset LSAN_OPTIONS
+fi
