c10f81b3ad80e30450723ce038f24fdb39a3bbc5
dasbd72 <twbd723@gmail.com>
2024-10-11 01:36:52 +0800

    refactor: seperate function definition

--- hw2-mpi-threads-mandelbrot-set/hw2.cc
+++ hw2-mpi-threads-mandelbrot-set/hw2.cc
@@ -9,42 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void write_png(const char* filename, int iters, int width, int height, const int* buffer) {
-    FILE* fp = fopen(filename, "wb");
-    assert(fp);
-    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    assert(png_ptr);
-    png_infop info_ptr = png_create_info_struct(png_ptr);
-    assert(info_ptr);
-    png_init_io(png_ptr, fp);
-    png_set_IHDR(png_ptr, info_ptr, width, height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
-                 PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-    png_set_filter(png_ptr, 0, PNG_NO_FILTERS);
-    png_write_info(png_ptr, info_ptr);
-    png_set_compression_level(png_ptr, 1);
-    size_t row_size = 3 * width * sizeof(png_byte);
-    png_bytep row = (png_bytep)malloc(row_size);
-    for (int y = 0; y < height; ++y) {
-        memset(row, 0, row_size);
-        for (int x = 0; x < width; ++x) {
-            int p = buffer[(height - 1 - y) * width + x];
-            png_bytep color = row + x * 3;
-            if (p != iters) {
-                if (p & 16) {
-                    color[0] = 240;
-                    color[1] = color[2] = p % 16 * 16;
-                } else {
-                    color[0] = p % 16 * 16;
-                }
-            }
-        }
-        png_write_row(png_ptr, row);
-    }
-    free(row);
-    png_write_end(png_ptr, NULL);
-    png_destroy_write_struct(&png_ptr, &info_ptr);
-    fclose(fp);
-}
+void write_png(const char* filename, int iters, int width, int height, const int* buffer);
 
 int main(int argc, char** argv) {
     /* detect how many CPUs are available */
@@ -91,4 +56,41 @@ int main(int argc, char** argv) {
     /* draw and cleanup */
     write_png(filename, iters, width, height, image);
     free(image);
+}
+
+void write_png(const char* filename, int iters, int width, int height, const int* buffer) {
+    FILE* fp = fopen(filename, "wb");
+    assert(fp);
+    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    assert(png_ptr);
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    assert(info_ptr);
+    png_init_io(png_ptr, fp);
+    png_set_IHDR(png_ptr, info_ptr, width, height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+                 PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+    png_set_filter(png_ptr, 0, PNG_NO_FILTERS);
+    png_write_info(png_ptr, info_ptr);
+    png_set_compression_level(png_ptr, 1);
+    size_t row_size = 3 * width * sizeof(png_byte);
+    png_bytep row = (png_bytep)malloc(row_size);
+    for (int y = 0; y < height; ++y) {
+        memset(row, 0, row_size);
+        for (int x = 0; x < width; ++x) {
+            int p = buffer[(height - 1 - y) * width + x];
+            png_bytep color = row + x * 3;
+            if (p != iters) {
+                if (p & 16) {
+                    color[0] = 240;
+                    color[1] = color[2] = p % 16 * 16;
+                } else {
+                    color[0] = p % 16 * 16;
+                }
+            }
+        }
+        png_write_row(png_ptr, row);
+    }
+    free(row);
+    png_write_end(png_ptr, NULL);
+    png_destroy_write_struct(&png_ptr, &info_ptr);
+    fclose(fp);
 }
