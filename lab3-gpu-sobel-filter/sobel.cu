5189a4194d46f3ce5c4ce1481ace3ac92dde8b08
dasbd72 <twbd723@gmail.com>
2024-10-28 22:44:11 +0800

    feat: implemented cuda version

--- lab3-gpu-sobel-filter/sobel.cu
+++ lab3-gpu-sobel-filter/sobel.cu
@@ -9,10 +9,18 @@
 #define MASK_X 5
 #define MASK_Y 5
 #define SCALE 8
+#define ADJUST_X ((MASK_X % 2) ? 1 : 0)
+#define ADJUST_Y ((MASK_Y % 2) ? 1 : 0)
+#define MASK_ADJ_X (MASK_X - ADJUST_X)
+#define MASK_ADJ_Y (MASK_Y - ADJUST_Y)
+#define START_X (MASK_X / 2)
+#define START_Y (MASK_Y / 2)
+#define BLOCK_X 16
+#define BLOCK_Y 8
 
-/* Hint 7 */
-// this variable is used by device
-int mask[MASK_N][MASK_X][MASK_Y] = {
+#define CLAMP_8BIT(x) (x > 255.0 ? 255 : x)
+
+__constant__ short mask[MASK_N][MASK_X][MASK_Y] = {
     {{-1, -4, -6, -4, -1},
      {-2, -8, -12, -8, -2},
      {0, 0, 0, 0, 0},
@@ -24,7 +32,7 @@ int mask[MASK_N][MASK_X][MASK_Y] = {
      {-4, -8, 0, 8, 4},
      {-1, -2, 0, 2, 1}}};
 
-void read_png(const char* filename, unsigned char** image, unsigned* height, unsigned* width, unsigned* channels) {
+void read_png(const char* filename, unsigned char** image, unsigned* height, unsigned* width, unsigned* channels, unsigned* height_pad, unsigned* width_pad) {
     unsigned char sig[8];
     FILE* fp = fopen(filename, "rb");
     fread(sig, 1, 8, fp);
@@ -38,15 +46,16 @@ void read_png(const char* filename, unsigned char** image, unsigned* height, uns
     png_read_info(png_ptr, info_ptr);
     int bit_depth, color_type;
     png_get_IHDR(png_ptr, info_ptr, width, height, &bit_depth, &color_type, NULL, NULL, NULL);
-    png_uint_32 i, rowbytes;
     png_bytep row_pointers[*height];
     png_read_update_info(png_ptr, info_ptr);
-    rowbytes = png_get_rowbytes(png_ptr, info_ptr);
     *channels = (int)png_get_channels(png_ptr, info_ptr);
-    *image = (unsigned char*)malloc(rowbytes * *height);
+    *height_pad = (*height % BLOCK_Y == 0) ? *height : (*height / BLOCK_Y + 1) * BLOCK_Y;
+    *width_pad = (*width % BLOCK_X == 0) ? *width : (*width / BLOCK_X + 1) * BLOCK_X;
+    size_t size = *channels * (*width_pad + MASK_ADJ_X) * (*height_pad + MASK_ADJ_Y) * sizeof(unsigned char);
+    *image = (unsigned char*)malloc(size);
     assert(*image);
-    for (i = 0; i < *height; ++i) {
-        row_pointers[i] = *image + i * rowbytes;
+    for (png_uint_32 i = 0; i < *height; ++i) {
+        row_pointers[i] = *image + (i + START_Y) * *channels * (*width_pad + MASK_ADJ_X) + START_X * *channels;
     }
     png_read_image(png_ptr, row_pointers);
     png_read_end(png_ptr, NULL);
@@ -54,7 +63,7 @@ void read_png(const char* filename, unsigned char** image, unsigned* height, uns
     fclose(fp);
 }
 
-void write_png(const char* filename, png_bytep image, const unsigned height, const unsigned width, const unsigned channels) {
+void write_png(const char* filename, png_bytep image, const unsigned height, const unsigned width, const unsigned channels, const unsigned height_pad, const unsigned width_pad) {
     FILE* fp = fopen(filename, "wb");
     png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     png_infop info_ptr = png_create_info_struct(png_ptr);
@@ -67,7 +76,7 @@ void write_png(const char* filename, png_bytep image, const unsigned height, con
     png_set_compression_level(png_ptr, 0);
     png_bytep row_ptr[height];
     for (int i = 0; i < height; ++i) {
-        row_ptr[i] = image + i * width * channels * sizeof(unsigned char);
+        row_ptr[i] = image + (i + START_Y) * channels * (width_pad + MASK_ADJ_X) * sizeof(unsigned char) + START_X * channels;
     }
     png_write_image(png_ptr, row_ptr);
     png_write_end(png_ptr, NULL);
@@ -75,84 +84,84 @@ void write_png(const char* filename, png_bytep image, const unsigned height, con
     fclose(fp);
 }
 
-/* Hint 5 */
-// this function is called by host and executed by device
-void sobel(unsigned char* s, unsigned char* t, unsigned height, unsigned width, unsigned channels) {
+__global__ void sobel(unsigned char* s, unsigned char* t, unsigned height, unsigned width, unsigned channels, unsigned height_pad, unsigned width_pad) {
     int x, y, i, v, u;
-    int R, G, B;
+    short R, G, B;
     double val[MASK_N * 3] = {0.0};
-    int adjustX, adjustY, xBound, yBound;
+    double totalR, totalG, totalB;
 
-    /* Hint 6 */
-    // parallel job by blockIdx, blockDim, threadIdx
-    for (y = 0; y < height; ++y) {
-        for (x = 0; x < width; ++x) {
-            for (i = 0; i < MASK_N; ++i) {
-                adjustX = (MASK_X % 2) ? 1 : 0;
-                adjustY = (MASK_Y % 2) ? 1 : 0;
-                xBound = MASK_X / 2;
-                yBound = MASK_Y / 2;
+    x = blockIdx.x * blockDim.x + threadIdx.x + START_X;
+    y = blockIdx.y * blockDim.y + threadIdx.y + START_Y;
+#pragma unroll 2
+    for (i = 0; i < MASK_N; ++i) {
+        val[i * 3 + 2] = 0.0;
+        val[i * 3 + 1] = 0.0;
+        val[i * 3] = 0.0;
 
-                val[i * 3 + 2] = 0.0;
-                val[i * 3 + 1] = 0.0;
-                val[i * 3] = 0.0;
-
-                for (v = -yBound; v < yBound + adjustY; ++v) {
-                    for (u = -xBound; u < xBound + adjustX; ++u) {
-                        if ((x + u) >= 0 && (x + u) < width && y + v >= 0 && y + v < height) {
-                            R = s[channels * (width * (y + v) + (x + u)) + 2];
-                            G = s[channels * (width * (y + v) + (x + u)) + 1];
-                            B = s[channels * (width * (y + v) + (x + u)) + 0];
-                            val[i * 3 + 2] += R * mask[i][u + xBound][v + yBound];
-                            val[i * 3 + 1] += G * mask[i][u + xBound][v + yBound];
-                            val[i * 3 + 0] += B * mask[i][u + xBound][v + yBound];
-                        }
-                    }
-                }
+#pragma unroll 5
+        for (v = -START_Y; v < START_Y + ADJUST_Y; ++v) {
+#pragma unroll 5
+            for (u = -START_X; u < START_X + ADJUST_X; ++u) {
+                R = s[channels * ((width_pad + MASK_ADJ_X) * (y + v) + (x + u)) + 2];
+                G = s[channels * ((width_pad + MASK_ADJ_X) * (y + v) + (x + u)) + 1];
+                B = s[channels * ((width_pad + MASK_ADJ_X) * (y + v) + (x + u)) + 0];
+                val[i * 3 + 2] += R * mask[i][u + START_X][v + START_Y];
+                val[i * 3 + 1] += G * mask[i][u + START_X][v + START_Y];
+                val[i * 3 + 0] += B * mask[i][u + START_X][v + START_Y];
             }
-
-            double totalR = 0.0;
-            double totalG = 0.0;
-            double totalB = 0.0;
-            for (i = 0; i < MASK_N; ++i) {
-                totalR += val[i * 3 + 2] * val[i * 3 + 2];
-                totalG += val[i * 3 + 1] * val[i * 3 + 1];
-                totalB += val[i * 3 + 0] * val[i * 3 + 0];
-            }
-
-            totalR = sqrt(totalR) / SCALE;
-            totalG = sqrt(totalG) / SCALE;
-            totalB = sqrt(totalB) / SCALE;
-            const unsigned char cR = (totalR > 255.0) ? 255 : totalR;
-            const unsigned char cG = (totalG > 255.0) ? 255 : totalG;
-            const unsigned char cB = (totalB > 255.0) ? 255 : totalB;
-            t[channels * (width * y + x) + 2] = cR;
-            t[channels * (width * y + x) + 1] = cG;
-            t[channels * (width * y + x) + 0] = cB;
         }
     }
+
+    totalR = 0.0;
+    totalG = 0.0;
+    totalB = 0.0;
+#pragma unroll 2
+    for (i = 0; i < MASK_N; ++i) {
+        totalR += val[i * 3 + 2] * val[i * 3 + 2];
+        totalG += val[i * 3 + 1] * val[i * 3 + 1];
+        totalB += val[i * 3 + 0] * val[i * 3 + 0];
+    }
+    totalR = sqrt(totalR) / SCALE;
+    totalG = sqrt(totalG) / SCALE;
+    totalB = sqrt(totalB) / SCALE;
+    t[channels * ((width_pad + MASK_ADJ_X) * y + x) + 2] = CLAMP_8BIT(totalR);
+    t[channels * ((width_pad + MASK_ADJ_X) * y + x) + 1] = CLAMP_8BIT(totalG);
+    t[channels * ((width_pad + MASK_ADJ_X) * y + x) + 0] = CLAMP_8BIT(totalB);
 }
 
 int main(int argc, char** argv) {
     assert(argc == 3);
-    unsigned height, width, channels;
+    char* input_filename = argv[1];
+    char* output_filename = argv[2];
+
+    size_t size;
+    unsigned height, width, channels, height_pad, width_pad;
     unsigned char* host_s = NULL;
-    read_png(argv[1], &host_s, &height, &width, &channels);
-    unsigned char* host_t = (unsigned char*)malloc(height * width * channels * sizeof(unsigned char));
+    unsigned char* host_t = NULL;
+    unsigned char* dev_s = NULL;
+    unsigned char* dev_t = NULL;
+    dim3 blk, grid;
 
-    /* Hint 1 */
-    // cudaMalloc(...) for device src and device dst
+    read_png(input_filename, &host_s, &height, &width, &channels, &height_pad, &width_pad);
 
-    /* Hint 2 */
-    // cudaMemcpy(...) copy source image to device (filter matrix if necessary)
+    blk = dim3(BLOCK_X, BLOCK_Y);
+    grid = dim3(width_pad / BLOCK_X, height_pad / BLOCK_Y);
+    size = channels * (width_pad + MASK_ADJ_X) * (height_pad + MASK_ADJ_Y) * sizeof(unsigned char);
+    host_t = (unsigned char*)malloc(size);
+    cudaMalloc(&dev_s, size);
+    cudaMalloc(&dev_t, size);
+    cudaMemset(dev_t, 0, size);
 
-    /* Hint 3 */
-    // acclerate this function
-    sobel(host_s, host_t, height, width, channels);
+    cudaMemcpy(dev_s, host_s, size, cudaMemcpyHostToDevice);
+    sobel<<<grid, blk>>>(dev_s, dev_t, height, width, channels, height_pad, width_pad);
+    cudaMemcpyAsync(host_t, dev_t, size, cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
 
-    /* Hint 4 */
-    // cudaMemcpy(...) copy result image to host
-    write_png(argv[2], host_t, height, width, channels);
+    write_png(output_filename, host_t, height, width, channels, height_pad, width_pad);
 
+    free(host_s);
+    free(host_t);
+    cudaFree(dev_s);
+    cudaFree(dev_t);
     return 0;
 }
