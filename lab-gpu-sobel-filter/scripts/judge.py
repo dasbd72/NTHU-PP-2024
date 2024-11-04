fda707bfa7e20ae8d436b5c4e562b8ec9d71ff86
dasbd72 <twbd723@gmail.com>
2024-11-04 23:27:41 +0800

    feat: add judge script

---
+++ lab-gpu-sobel-filter/scripts/judge.py
@@ -0,0 +1,38 @@
+import os
+import signal
+import shutil
+import time
+
+from .build import build
+
+
+def signal_int(signum, frame):
+    print("Exiting...")
+    exit(0)
+
+
+def copy_move():
+    # Copy files to a temporary directory and change directory
+    judge_dir = os.path.expanduser("~/.tmp.judge")
+    shutil.rmtree(judge_dir, ignore_errors=True)
+    os.mkdir(judge_dir)
+    shutil.copy("Makefile", judge_dir)
+    shutil.copy("modules.list", judge_dir)
+    shutil.copy("sobel.cu", judge_dir)
+    os.chdir(judge_dir)
+    print(f"Changed directory to {judge_dir}")
+
+
+def judge_loop():
+    code = build()
+    if code != 0:
+        raise Exception(f"Build failed with code {code}")
+    while True:
+        os.system("lab-sobel-opt-judge")
+        time.sleep(301)
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_int)
+    copy_move()
+    judge_loop()
